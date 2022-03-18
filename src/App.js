cb9d66a646e92a2f6c11f0bc757b3af0c331543b
masashi kishimoto <drehbleistift@gmail.com>
2022-03-18 18:52:36 +0900

    map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  GoogleMap,
+  LoadScript,
+  InfoWindow,
+  Marker,
+} from "@react-google-maps/api";
+import geos from "./agg.json";
+import { useState } from "react";
+
+const containerStyle = {
+  height: "100vh",
+  width: "100%",
+};
+
+const center = {
+  lat: 35.6598003,
+  lng: 139.7023894,
+};
 
 function App() {
+  const [selectedPlace, setSelectedPlace] = useState(null);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadScript googleMapsApiKey="api key">
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={13}>
+          {geos.map((geo, index) => (
+            <Marker
+              key={index}
+              position={{
+                lat: geo.geojson.geometry.coordinates[1],
+                lng: geo.geojson.geometry.coordinates[0],
+              }}
+              onClick={() => {
+                setSelectedPlace(geo);
+              }}
+            />
+          ))}
+          {selectedPlace && (
+            <InfoWindow
+              onCloseClick={() => {
+                setSelectedPlace(null);
+              }}
+              position={{
+                lat: selectedPlace.geojson.geometry.coordinates[1],
+                lng: selectedPlace.geojson.geometry.coordinates[0],
+              }}
+            >
+              <div>
+                {selectedPlace.freq_words.map((word_count, index) => (
+                  <p style={{ fontSize: 15 }} key={index}>
+                    {word_count.word}: {word_count.count}
+                  </p>
+                ))}
+              </div>
+            </InfoWindow>
+          )}
+        </GoogleMap>
+      </LoadScript>
     </div>
   );
 }
